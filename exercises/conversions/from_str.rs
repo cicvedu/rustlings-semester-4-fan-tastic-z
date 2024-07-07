f6fd473701fedc80e8c913ac045b1c7b7e5df0aa
fan-tastic-z <fantastic.fun.zf@gmail.com>
2024-07-07 15:29:19 +0800

    feat: 20240707 finished

--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -31,7 +31,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
 
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
@@ -52,6 +51,27 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len() == 0 {
+            return Err(ParsePersonError::Empty);
+        }
+        let mut split_res = s.split(',');
+
+        if split_res.clone().count() != 2 {
+            return Err(ParsePersonError::BadLen);
+        }
+        let name = split_res.nth(0).unwrap_or("");
+        if name.is_empty() {
+            return Err(ParsePersonError::NoName);
+        }
+        let age = split_res.nth(0).unwrap_or("").parse::<usize>();
+        if let Err(e) = age {
+            return Err(ParsePersonError::ParseInt(e));
+        }
+
+        Ok(Person {
+            name: name.to_string(),
+            age: age.unwrap()
+        })
     }
 }
 
